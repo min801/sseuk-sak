a4897fb5bd49f64f38678588985aee18a3ef5421
min801 <miningfulo6425@gmail.com>
2026-06-13 18:01:08 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { db } from './firebase';
+import { collection, onSnapshot } from 'firebase/firestore';
 
 export default function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, "posts"), (snapshot) => {
+      setData(snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })));
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <div style={{ padding: '50px', textAlign: 'center' }}>
+    <div style={{ padding: '50px' }}>
       <h1>슥삭(Sseuk-Sak)</h1>
-      <p>여기에 낙서를 시작합니다.</p>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
