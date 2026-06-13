ba92d35105d75cec5f65b91cbfddf59a203c8b97
min801 <miningfulo6425@gmail.com>
2026-06-13 17:54:36 +0900

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>슥삭(Sseuk-Sak)</h1>
+      <p>여기에 낙서를 시작합니다.</p>
+    </div>
+  );
+}
